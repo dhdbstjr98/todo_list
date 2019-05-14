35d46a4b3265ee3e368d94c201d45515354240ca
오윤석 <dhdbstjr98@khu.ac.kr>
2019-05-14 18:03:22 +0900

    make left menu layout (rough)

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(function() {
+	M.AutoInit();
+	$(".menu-button").click(function() {
+		$(".left-menu").toggleClass("active");
+	});
+	$(".left-menu .input-field input").on("focus", function() {
+		$(this).parents(".left-menu .input-field").addClass("focus");
+		$(this).parents(".left-menu").addClass("focus");
+	});
+	$(".left-menu .input-field input").on("blur", function() {
+		$(this).parents(".left-menu .input-field").removeClass("focus");
+		$(this).parents(".left-menu").removeClass("focus");
+	});
+});
